db13ee639fee43971f7fd0e28904c6c3c7208ca2
camillamacedosenai <camillamacedosenai@gmail.com>
2026-02-26 17:24:57 -0300

    Add files via upload

---
+++ AULA_2.py
@@ -0,0 +1,11 @@
+# interpretador vai ignorar tudo que possuir uma # na frente
+
+# instalamos a linguagem de programação
+
+# verificar se a linguagem e as ferramentas
+
+print ('Hello world...')
+print ('Teste')
+
+
+#dados primitivos em python
---
+++ dados_primitivos_python.py
@@ -0,0 +1,23 @@
+#dados primitivos em python
+
+# tipo string - texto
+'texto'
+"texto 2 lkhkjsdhhkasdh"
+
+#tipo interger - inteiro
+
+print(1 + 2)
+1
+2
+100
+
+# tipo float - real
+
+print (5.0 - 102.10)
+2.10
+100.
+0.5
+
+# tipo bool - lógico
+True
+False 
