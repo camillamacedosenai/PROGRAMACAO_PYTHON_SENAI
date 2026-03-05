0a6a7970c3ca1460427a04b2df03d2487830205a
camillamacedosenai <camillamacedosenai@gmail.com>
2026-03-05 16:32:28 -0300

    Add files via upload

---
+++ variaveis/variaveis.py
@@ -0,0 +1,115 @@
+print()
+n1 = 4
+n2 = 6
+somaa = (n1*n2)
+resultadoa = somaa/2
+print("O resultado é", resultadoa)
+print()
+
+n3 = 5 
+n4 = 7
+resultadob = n3 + n4
+print('O resultado é', resultadob)
+print()
+
+n5 = 3
+resultadoc = n5**4
+print("O resultado é", resultadoc)
+print()
+
+n6 = 8
+n7 = 12
+n8 = 15
+somad = n6 + n7 + n8
+resultadod = somad/3
+print("O resultado é", resultadod)
+print()
+
+n9 = 2
+n10 = 10
+subtracaoe = n9 - n10
+resultadoe = subtracaoe*5
+print('O resultado é', resultadoe)
+print()
+
+n11 = 27
+n12 = 3
+divisaof = n11/n12
+resultadof = divisaof+5
+print("O resultado é", resultadof)
+print()
+
+n13 = 17
+n14 = 4
+módulog = n13%n14
+print("O módulo é", módulog)
+print()
+
+n15 = 9
+n16 = 3
+multiplicacaoh = n15*n16
+resultadoh = multiplicacaoh**2
+print('O resultado é', resultadoh)
+print()
+
+n17 = 81
+raiz = n17**0.5
+print('A raiz quadrada de 81 é', raiz)
+print()
+
+n18 = 20
+n19 = 3
+n20 = 4
+multiplicacaoi = n19*n20
+resultadoi = multiplicacaoi + n18
+print('O resultado é', resultadoi)
+print()
+
+n21 = 15
+n22 = 2
+multiplicacaoj = n21*n22
+resultadoj = multiplicacaoj-7
+print('O resultado é', resultadoj)
+print()
+
+n23 = 5
+resultadok = n23**3
+print('O resultado é', resultadok)
+print()
+
+n24 = 17
+n25 = 21
+n26 = 25
+somal = n24 + n25 + n26
+resultadol = somal/3
+print('O resultado é', resultadol)
+print()
+
+n27 = 11
+n28 = 2
+multiplicacaom = n27*n28
+resultadom = multiplicacaom+7
+print('O resultado é', resultadom)
+print()
+
+n29 = 15
+n30 = 3
+n31 = 8
+multiplicacaon = n30*n31
+subtracaon = multiplicacaon - n29 
+resultadon = subtracaon/2
+print("O resultado é", resultadon)
+print()
+
+n32 = 2
+resultadoo = n32**10
+print('O resultado é', resultadoo)
+print()
+
+
+
+
+
+
+
+
