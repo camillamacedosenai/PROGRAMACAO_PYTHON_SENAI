a891e412fcfe0673a6bc2853e31ad4aef7c1534d
camillamacedosenai <camillamacedosenai@gmail.com>
2026-03-06 14:30:44 -0300

    Add files via upload

---
+++ listas/listas.py
@@ -0,0 +1,25 @@
+print()
+lista = list(range(2,21,2))
+print(lista)
+print()
+
+números = list(range(1,11))
+print(números)
+print()
+
+print(números[2])
+print()
+
+números.append(9)
+print(números)
+print()
+
+números.remove(5)
+print(números)
+print()
+
+carros = ['gol', 'voyage', 'civic']
+print(carros)
+print()
+
+print(f'{números}{carros}')
